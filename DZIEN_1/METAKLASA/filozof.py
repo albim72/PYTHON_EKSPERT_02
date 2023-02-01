5c8ce011f5058ff17d9146dee7cad20ff7bd6071
albim72 <marcin.albiniak72@gmail.com>
2023-02-01 14:18:56 +0100

    Update filozof.py

--- DZIEN_1/METAKLASA/filozof.py
+++ DZIEN_1/METAKLASA/filozof.py
@@ -7,13 +7,20 @@ else:
 def odpowiedz(self):
     return "Tak! Ziemia jest płaska!"
 
+def odpowiedz_new(self):
+    return "Nie! Ziemia jest Elipsoidą..."
+
 def brak(self):
     return "brak odpowiedzi..."
 
 class SednoOdpowiedzi(type):
     def __init__(cls,clsname,bases,attrs):
+
         if required:
-            cls.odpowiedz = odpowiedz
+            if clsname == "Kopernik":
+                cls.odpowiedz = odpowiedz_new
+            else:
+                cls.odpowiedz = odpowiedz
         else:
             cls.odpowiedz = brak
 
@@ -28,10 +35,17 @@ class Platon(metaclass=SednoOdpowiedzi):
 class SwTomasz(metaclass=SednoOdpowiedzi):
     pass
 
+class Kopernik(metaclass=SednoOdpowiedzi):
+    pass
+
 fil1 = Arystoteles()
 fil2 = Platon()
 fil3 = SwTomasz()
+fil4 = Kopernik()
 
 print(f"Filozof {fil1.__class__.__name__} mówi: {fil1.odpowiedz()}")
 print(f"Filozof {fil2.__class__.__name__} mówi: {fil2.odpowiedz()}")
 print(f"Filozof {fil3.__class__.__name__} mówi: {fil3.odpowiedz()}")
+#odpowiedź Kopernika -> Nie! Ziemia jest Elipsoidą...
+#zachowaj takie elementy: nazwa metody -> odpowiedz, budowa na metaklasie SednoOdpowiedzi
+print(f"Filozof {fil4.__class__.__name__} mówi: {fil4.odpowiedz()}")
