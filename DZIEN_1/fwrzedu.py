330671c4cdf122147d33ab0d68bd6e7536519eea
albim72 <marcin.albiniak72@gmail.com>
2023-02-01 10:02:52 +0100

    Create fwrzedu.py

---
+++ DZIEN_1/fwrzedu.py
@@ -0,0 +1,5 @@
+#użyj funkcji wyższego rzędu filter do wyfiltrwania
+# dowolnej kolekcji po warunku parzystości (tylko liczby parzyste)
+
+n1 = [563,56,1,2,4,88,9,112,0,-99,24,5,6,7,18,90]
+n2 = (6,8,9,-1,0,8,4,12,3,56,7,9,1,2,)
