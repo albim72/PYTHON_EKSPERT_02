d42900290a22dd127c9fd45e48f24f0b8fb906d4
albim72 <marcin.albiniak72@gmail.com>
2023-02-02 10:15:12 +0100

    Create grade.py

---
+++ DZIEN_2/EGZAMINY/grade.py
@@ -0,0 +1,17 @@
+class Grade:
+    def __init__(self):
+        self._value = 0
+        
+    def __get__(self, instance, owner):
+        return self._value
+    
+    def __set__(self, instance, value):
+        if not (0 <= value <= 100):
+            raise ValueError('Ocena musi być z zakresu 0-100!')
+        self._value = value
+        
+        
+class Exams:
+    math_grade = Grade()
+    writing_grade = Grade()
+    science_grade = Grade()
