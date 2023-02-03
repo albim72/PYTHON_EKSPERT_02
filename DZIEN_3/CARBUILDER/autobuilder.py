b49b5e9a1cd690e868470e88ca73fe958f5e6233
albim72 <marcin.albiniak72@gmail.com>
2023-02-03 08:22:11 +0100

    Update autobuilder.py

--- DZIEN_3/CARBUILDER/autobuilder.py
+++ DZIEN_3/CARBUILDER/autobuilder.py
@@ -28,7 +28,7 @@ class Car:
         print(f'rodzaj pojazdu: {self._body.shape}')
         print(f'silnik [kM]: {self._engine.horsepower}')
         print(f'średnica koła [cal]: {self._wheels[0].size}')
-        
+
 #Bulider
 class Builder(ABC):
     @abstractmethod
@@ -39,6 +39,28 @@ class Builder(ABC):
 
     @abstractmethod
     def getBody(self): pass
-    
+
 #Director
 
+class Director:
+    __builder = None
+    
+    def setBuilder(self,builder):
+        self.__builder = builder
+        
+    def getCar(self):
+        car = Car()
+        
+        body = self.__builder.getBody()
+        car.setBody(body)
+
+        engine = self.__builder.getEngine()
+        car.setEngine(engine)
+        
+        i=0
+        while i<4:
+            wheel = self.__builder.getWheel()
+            car.attachWheel(wheel)
+            i+=1
+        return car
+    
