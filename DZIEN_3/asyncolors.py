93edf324f201be61a0391e33cfd187a8e07ef4b9
albim72 <marcin.albiniak72@gmail.com>
2023-02-03 09:34:59 +0100

    Create asyncolors.py

---
+++ DZIEN_3/asyncolors.py
@@ -0,0 +1,30 @@
+import asyncio
+import random
+
+#ANSI colors
+c = (
+    "\033[0m",
+    "\033[36m",
+    "\33[91m",
+    "\33[35m"
+)
+
+async def makerandom(idx:int, threshold:int=6) -> int:
+    print(f"{c[idx+1]} inicjacja makerandom({idx}).")
+    i = random.randint(0,10)
+    while i <= threshold:
+        print(f"{c[idx+1]} makrandom({idx}=={i}) jest zbyt niska, powtórzenie...")
+        await asyncio.sleep(idx+1)
+        i = random.randint(0,10)
+    print(f"{c[idx + 1]} zakończono: makrandom({idx}=={i}) {c[0]}")
+    return i
+
+async def main():
+    res = await asyncio.gather(*(makerandom(i,9-i) for i in range(3)))
+    return res
+
+if __name__ == '__main__':
+    random.seed(444)
+    r1,r2,r3 = asyncio.run(main())
+    print("\n")
+    print(f'r1: {r1}, r2: {r2}, r3: {r3}')
